62ddc627d29c5dde8a8ce148648a18d1e9d97f8f
Jingwei Zhao <jingwei.zhao0527@gmail.com>
2023-06-22 13:23:32 -0400

    Add some components folder

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { loginFields } from "../../constants/formFields";
 import Input from "../Input/Input";
